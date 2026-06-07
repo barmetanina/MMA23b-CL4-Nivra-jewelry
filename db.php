266e58f8ad0bdc2d526aef6ce2cacbc1313a84cd
mariadelapazschibli-spec <mariadelapaz.schibli@gmail.com>
2026-06-07 12:09:19 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,1 @@
-<?php
-
-$host = "localhost";
-$dbname = "webshop-nivra";
-$user = "maria-schibli";
-$password = "8z_L5Wemwzz8%Ecs";
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("DB Verbindung fehlgeschlagen: " . $e->getMessage());
-}
-
-?>
+file löschen
